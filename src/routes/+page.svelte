4925bd9f24a8c54a15254b1fe5035fdb64fc36f1
Toxin Nozaki <136707209+ToxinNozaki@users.noreply.github.com>
2024-11-24 00:11:44 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -46,7 +46,7 @@
       <div class="flex items-center justify-between p-2">
         <p class="text-sm text-slate-500">
           <a
-            href="https://www.ansonh.com"
+            href="https://www.stake.us"
             target="_blank"
             rel="noreferrer"
             class=" text-cyan-600 transition hover:text-cyan-500"
@@ -56,7 +56,7 @@
           © 2024
         </p>
         <a
-          href="https://github.com/AnsonH/plinko-game"
+          href="https://stake.us/casino/games/plinko"
           target="_blank"
           rel="noreferrer"
           class="flex items-center gap-1 p-1 text-sm text-slate-500 transition hover:text-cyan-500"
